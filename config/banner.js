2c2c8ae49b9a4021549c6cad212cfb769175c2bb
Airdrop Legion Community <bandijalikon@gmail.com>
2025-04-06 02:05:55 +0700

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -1,16 +1,18 @@
 const figlet = require('figlet');
 const chalk = require('chalk').default;
 
-function displayBanner(){
-    const banner = figlet.textSync ('BlockScout', {
-        font: 'ANSI Shadow',
+function displayBanner() {
+    const banner = figlet.textSync('Airdrop Legion Community', {
+        font: 'Slant',
         horizontalLayout: 'default',
-        verticalLayout: 'default'
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: false
     })
-    console.log(chalk.green(banner))
-    console.log(chalk.yellow('========================================='));
-    console.log(chalk.magenta('Github   : https://github.com/0x-Disciple'));
-    console.log(chalk.magenta('Telegram : https://t.me/CryptoKidzs'));
-    console.log(chalk.yellow('========================================='))
+    console.log(chalk.green(banner));
+    console.log(chalk.yellow('===================================================='));
+    console.log(chalk.magenta('Github   : https://github.com/AirdropLegionCommunity'));
+    console.log(chalk.magenta('Telegram : https://t.me/airdropalc'))
+    console.log(chalk.yellow('===================================================='));
 }
-displayBanner();
+displayBanner()
